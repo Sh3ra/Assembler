bee531e6832c622724f7344b731559187f777b20
sh3ra <youssefalshaarawi@yahoo.com>
2020-05-28 18:12:38 +0200

    TA is missing

--- main.cpp
+++ main.cpp
@@ -241,87 +241,86 @@ string HexToBin(string hexdec)
     return res;
 }
 
-void createMap(unordered_map<string, char> *um) 
-{ 
-    (*um)["0000"] = '0'; 
-    (*um)["0001"] = '1'; 
-    (*um)["0010"] = '2'; 
-    (*um)["0011"] = '3'; 
-    (*um)["0100"] = '4'; 
-    (*um)["0101"] = '5'; 
-    (*um)["0110"] = '6'; 
-    (*um)["0111"] = '7'; 
-    (*um)["1000"] = '8'; 
-    (*um)["1001"] = '9'; 
-    (*um)["1010"] = 'A'; 
-    (*um)["1011"] = 'B'; 
-    (*um)["1100"] = 'C'; 
-    (*um)["1101"] = 'D'; 
-    (*um)["1110"] = 'E'; 
-    (*um)["1111"] = 'F'; 
-} 
-  
-// function to find hexadecimal  
-// equivalent of binary 
-string binToHex(string bin) 
-{ 
-    int l = bin.size(); 
-    int t = bin.find_first_of('.'); 
-      
-    // length of string before '.' 
-    int len_left = t != -1 ? t : l; 
-      
-    // add min 0's in the beginning to make 
-    // left substring length divisible by 4  
-    for (int i = 1; i <= (4 - len_left % 4) % 4; i++) 
-        bin = '0' + bin; 
-      
-    // if decimal point exists     
-    if (t != -1)     
-    { 
-        // length of string after '.' 
-        int len_right = l - len_left - 1; 
-          
-        // add min 0's in the end to make right 
-        // substring length divisible by 4  
-        for (int i = 1; i <= (4 - len_right % 4) % 4; i++) 
-            bin = bin + '0'; 
-    } 
-      
-    // create map between binary and its 
-    // equivalent hex code 
-    unordered_map<string, char> bin_hex_map; 
-    createMap(&bin_hex_map); 
-      
-    int i = 0; 
-    string hex = ""; 
-      
-    while (1) 
-    { 
-        // one by one extract from left, substring 
-        // of size 4 and add its hex code 
-        hex += bin_hex_map[bin.substr(i, 4)]; 
-        i += 4; 
-        if (i == bin.size()) 
-            break; 
-              
-        // if '.' is encountered add it 
-        // to result 
-        if (bin.at(i) == '.')     
-        { 
-            hex += '.'; 
-            i++; 
-        } 
-    } 
-      
-    // required hexadecimal number 
-    return hex;     
+void createMap(unordered_map<string, char> *um)
+{
+    (*um)["0000"] = '0';
+    (*um)["0001"] = '1';
+    (*um)["0010"] = '2';
+    (*um)["0011"] = '3';
+    (*um)["0100"] = '4';
+    (*um)["0101"] = '5';
+    (*um)["0110"] = '6';
+    (*um)["0111"] = '7';
+    (*um)["1000"] = '8';
+    (*um)["1001"] = '9';
+    (*um)["1010"] = 'A';
+    (*um)["1011"] = 'B';
+    (*um)["1100"] = 'C';
+    (*um)["1101"] = 'D';
+    (*um)["1110"] = 'E';
+    (*um)["1111"] = 'F';
+}
+
+// function to find hexadecimal
+// equivalent of binary
+string binToHex(string bin)
+{
+    int l = bin.size();
+    int t = bin.find_first_of('.');
+
+    // length of string before '.'
+    int len_left = t != -1 ? t : l;
+
+    // add min 0's in the beginning to make
+    // left substring length divisible by 4
+    for (int i = 1; i <= (4 - len_left % 4) % 4; i++)
+        bin = '0' + bin;
+
+    // if decimal point exists
+    if (t != -1)
+    {
+        // length of string after '.'
+        int len_right = l - len_left - 1;
+
+        // add min 0's in the end to make right
+        // substring length divisible by 4
+        for (int i = 1; i <= (4 - len_right % 4) % 4; i++)
+            bin = bin + '0';
+    }
+
+    // create map between binary and its
+    // equivalent hex code
+    unordered_map<string, char> bin_hex_map;
+    createMap(&bin_hex_map);
+
+    int i = 0;
+    string hex = "";
+
+    while (1)
+    {
+        // one by one extract from left, substring
+        // of size 4 and add its hex code
+        hex += bin_hex_map[bin.substr(i, 4)];
+        i += 4;
+        if (i == bin.size())
+            break;
+
+        // if '.' is encountered add it
+        // to result
+        if (bin.at(i) == '.')
+        {
+            hex += '.';
+            i++;
+        }
+    }
+
+    // required hexadecimal number
+    return hex;
 }
 int get_TA(string op)
 {
-
-} 
-string get_address(string op, string opernad, dataTypesHandler zattout,int lctr)
+}
+string get_address(string op, string opernad, dataTypesHandler zattout, int lctr)
 {
     string disp = "";
     string bin = HexToBin(op);
@@ -355,27 +354,29 @@ string get_address(string op, string opernad, dataTypesHandler zattout,int lctr)
         d = get_TA(opernad) - lctr;
     }
     disp += decToHexa(d);
-    if (op[0] == '+'){
+    if (op[0] == '+')
+    {
         bin += "1";
-        while (disp.size()<5)
+        while (disp.size() < 5)
         {
-            disp="0"+disp;
+            disp = "0" + disp;
         }
-        disp=disp.substr(0,5);
+        disp = disp.substr(0, 5);
     }
-    else{
+    else
+    {
         bin += "0";
-        while (disp.size()<3)
+        while (disp.size() < 3)
         {
-            disp="0"+disp;
+            disp = "0" + disp;
         }
-        disp=disp.substr(0,3);
+        disp = disp.substr(0, 3);
     }
-    return binToHex(bin)+disp;
+    return binToHex(bin) + disp;
 }
 
 bool base = false;
-int baseR=0;
+int baseR = 0;
 bool contain_comma(string s)
 {
     for (char x : s)
@@ -425,10 +426,15 @@ void writeobjCode(vector<vector<string>> code)
             lctr += he5o.second;
             line += he5o.first;
         }
-        else if (code[i][1]=="base"&&code[i][0] == "")
+        else if (code[i][1] == "base" && code[i][0] == "")
         {
-            base=true;
-            baseR=getHex(zattout.symbolicTable[code[i][2]]);
+            base = true;
+            baseR = getHex(zattout.symbolicTable[code[i][2]]);
+        }
+        else if (code[i][1] == "nobase" && code[i][0] == "")
+        {
+            base = false;
+            baseR = 0;
         }
         else if (code[i][0] == "")
         {
@@ -440,7 +446,7 @@ void writeobjCode(vector<vector<string>> code)
             }
             else
             {
-                line += get_address(code[i][1], code[i][2], zattout,lctr);
+                line += get_address(code[i][1], code[i][2], zattout, lctr);
             }
         }
         Slctr = decToHexa(lctr);
